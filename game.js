16f6c017e80aa425e6952568a4c35805615e50ba
Kodeinekote <torlovarabota@gmail.com>
2026-03-11 18:27:51 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,7 @@
+let playerPosition = 0
 
+const board = document.getElementById("game-board")
+
+const rollButton = document.getElementById("rollDice")
+
+const posText = document.getElementById("playerPos")
