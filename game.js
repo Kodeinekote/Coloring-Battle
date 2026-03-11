8a4b51e18ffdab8b0344b8db0669ba353821bcf2
Kodeinekote <torlovarabota@gmail.com>
2026-03-11 18:32:49 +0300

    Update game.js

--- game.js
+++ game.js
@@ -5,3 +5,25 @@ const board = document.getElementById("game-board")
 const rollButton = document.getElementById("rollDice")
 
 const posText = document.getElementById("playerPos")
+
+for(let i=0;i<50;i++){ //for - это цикл, 50 - это 50 клеток
+
+let cell = document.createElement("div")
+
+cell.className="cell"
+
+cell.innerText=i
+
+board.appendChild(cell)
+
+}
+
+rollButton.onclick = function(){ //а это уже кубик - ролл
+
+let dice = Math.floor(Math.random()*6)+1
+
+playerPosition += dice
+
+posText.innerText = playerPosition
+
+}
